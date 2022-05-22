62025dfad7179bba134bf6d431ab00c7a5bedc70
Bruno Barange <bruno.akun@gmail.com>
2022-05-22 09:38:31 +0200

    Nivel 1 ejercicio 2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-getAllDirectors(movies);
+// Ejercicio 01
+
+/*
+  getAllDirectors(movies);
+*/
+
+// Ejercicio 02
+/*
+let aux = getMoviesFromDirector(movies, 'Quentin Tarantino');
+console.log(aux);
+*/
